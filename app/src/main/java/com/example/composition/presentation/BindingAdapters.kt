0e9050f112d66c4217a6756236e48c00cf4da6ce
NoSentToEmail <saniamax20001@gmail.com>
2023-07-17 15:05:54 +0300

    рефакторинг с Data Binding и  Binding Adapters, почищеный код, переделаные хмл файлы

--- app/src/main/java/com/example/composition/presentation/BindingAdapters.kt
+++ app/src/main/java/com/example/composition/presentation/BindingAdapters.kt
@@ -1,7 +1,11 @@
 package com.example.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.composition.R
 import com.example.composition.domain.entity.GameResult
@@ -59,3 +63,38 @@ fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
 //        R.drawable.ic_sad
 //    }
 //}
+
+@BindingAdapter("enoughCountOfRightAnswers")
+fun bindEnoughCount(textView: TextView, enough: Boolean){
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("enoughPercentOfRightAnswers")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean){
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int{
+    val colorResId = if(goodState){
+        android.R.color.holo_green_light
+    }else{
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int){
+    textView.text = number.toString()
+}
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickLisneter: OnOptionClickListener){
+    textView.setOnClickListener{
+        clickLisneter.onOptionClick(textView.text.toString().toInt())
+    }
+}
